f456da0e7d44b5879ddac8654cc762880f881f63
Gopal Ingle <gopalingle31102002@gmail.com>
2026-02-09 03:45:23 +0530

    fourth commit

---
+++ EP04_TalkIsCheap/App.js
@@ -0,0 +1,185 @@
+/* 
+- Header
+    - Logo
+    - Nav Items
+- Body
+    - Search
+    - Restaurant Container
+        - Restaurant Cards
+            - Img
+            - Name of Restaurant
+            - Rating
+            - Cuisines
+            - Delivery time
+- Footer
+    - Copyright
+    - Links
+    - Address
+    - Contact Info
+*/
+
+
+import React from "react";
+import ReactDOM from "react-dom/client"
+
+
+const Header = () => {
+    return (
+        <div className="header">
+             <div className="logo-container"> 
+                <img className="logo" src="https://img.freepik.com/free-vector/food-shopping-logo-template-design_460848-10299.jpg"></img>
+             </div>
+             <div className="nav-items">
+                <ul>
+                    <li>Home</li>
+                    <li>About</li>
+                    <li>Contact</li>
+                    <li>Cart</li>
+                </ul>
+             </div>
+        </div>
+    )
+}
+
+const styleCard = {
+    backgroundColor: "#f0f0f0"
+}
+//Inline CSS in JSX is written using JS object
+//style={{backgroundColor: "#f0f0f0"}}, we can also directly write like this inside the JSX, first bracket {} is telling that JS is inside JSX and second bracket {backgroundColor: "#f0f0f0"} is that JS object
+
+const restaurantList = [
+    {
+        resName: "Pizza Hut",
+        id: 1,
+        cuisines: "Italian, Desi Spice, Asian",
+        rating: "4.5 ⭐️",
+        deliveryTime: "40 minutes",
+        src: "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/RX_THUMBNAIL/IMAGES/VENDOR/2025/9/1/65c716f7-09b0-42cb-b70f-3908542620f0_340854.JPG"
+    },
+    {
+        resName: "KFC",
+        id: 2,
+        cuisines: "Burger, Chicken, Fries",
+        rating: "4.8 ⭐️",
+        deliveryTime: "35 minutes",
+        src: "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/RX_THUMBNAIL/IMAGES/VENDOR/2026/1/8/36fb4603-86ff-4b69-9659-3555340b83c1_640360.JPG"
+    },
+    {
+        resName: "Domino's Pizza",
+        id: 3,
+        cuisines: "Pizza, Fast Food",
+        rating: "4.4 ⭐️",
+        deliveryTime: "30 minutes",
+        src: "https://upload.wikimedia.org/wikipedia/commons/7/74/Dominos_pizza_logo.svg"
+    },
+    {
+        resName: "Burger King",
+        id: 4,
+        cuisines: "Burger, American",
+        rating: "4.3 ⭐️",
+        deliveryTime: "32 minutes",
+        src: "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/2b4f62d606d1b2bfba9ba9e5386fabb7"
+    },
+    {
+        resName: "Subway",
+        id: 5,
+        cuisines: "Healthy, Sandwich",
+        rating: "4.2 ⭐️",
+        deliveryTime: "28 minutes",
+        src: "https://upload.wikimedia.org/wikipedia/commons/5/5c/Subway_2016_logo.svg"
+    },
+    {
+        resName: "McDonald's",
+        id: 6,
+        cuisines: "Burger, Beverages, Fries",
+        rating: "4.6 ⭐️",
+        deliveryTime: "25 minutes",
+        src: "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/535fc9f9c135f7982317bbb6a64a1ffc"
+    },
+    {
+        resName: "Haldiram's",
+        id: 7,
+        cuisines: "Indian, Sweets, Snacks",
+        rating: "4.7 ⭐️",
+        deliveryTime: "38 minutes",
+        src: "https://vectorseek.com/wp-content/uploads/2021/12/1970-Haldirams-Logo-vector.png"
+    },
+    {
+        resName: "Bikanervala",
+        id: 8,
+        cuisines: "North Indian, Sweets",
+        rating: "4.3 ⭐️",
+        deliveryTime: "42 minutes",
+        src: "https://upload.wikimedia.org/wikipedia/en/thumb/9/9e/Bikanervala_logo.svg/500px-Bikanervala_logo.svg.png"
+    },
+    {
+        resName: "Wow! Momo",
+        id: 9,
+        cuisines: "Momos, Chinese, Fast Food",
+        rating: "4.1 ⭐️",
+        deliveryTime: "27 minutes",
+        src: "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/6e04be27387483a7c00444f8e8241108"
+    },
+    {
+        resName: "Barbeque Nation",
+        id: 10,
+        cuisines: "BBQ, North Indian, Grill",
+        rating: "4.6 ⭐️",
+        deliveryTime: "50 minutes",
+        src: "https://images.seeklogo.com/logo-png/28/1/barbeque-nation-logo-png_seeklogo-287122.png"
+    }
+];
+
+const RestaurantCard = (props) => { //props is a JavaScript object that stores all attributes (data) passed from a parent component to a child component.
+    
+    //const {resName, cuisines, rating, deliveryTime, src} = props 
+    //Here we destructure the props object so that we can use props.resName directly as resName, similarly for others i.e. {props.resName} => {resName} 
+
+    const {resData} = props
+    const {resName, cuisines, rating, deliveryTime, src} = resData
+
+    return (
+        <div className="restaurant-card" style={styleCard}>
+            <img className="restaurant-logo" src={src}></img>
+            <h3>{resName}</h3>
+            <h4>{cuisines}</h4>
+            <h5>{rating}</h5>
+            <h5>{deliveryTime}</h5 >
+        </div>
+    )
+} 
+
+const Body = () => {
+    return (
+        <div className="body">
+            <div className="search">Search</div>
+            <div className="restaurant-container">
+                {restaurantList.map((restaurant) => (
+                    <RestaurantCard key={restaurant.id} resData={restaurant}/>
+                ))}     
+                {restaurantList.map((restaurant, index) => (
+                    <RestaurantCard key={index} resData={restaurant}/>
+                ))}    
+            </div>
+        </div>
+    )
+}
+
+const AppLayout = () => {
+    return (
+        <div className="app">
+             <Header/>
+             <Body/> 
+        </div>
+    )
+}
+
+ 
+const root = ReactDOM.createRoot(document.getElementById("root"))
+root.render(<AppLayout/>)
+
+
+//IMP-> Config driven UI = UI containing data according to different scenario, like in swiggy api the UI is rendering different data for different locations
+//Key is needed so React can uniquely identify each element among its siblings to update the UI efficiently.
+//React only looks at keys within the current list / current siblings, not the whole app.
+//Not using key (not acceptable) <<<< using index as a key (not good practice) <<<< using unique id as a key (best practice)
