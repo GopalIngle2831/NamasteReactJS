3390e1961fdc589fabd4d7b927839f3e8df001a9
Gopal Ingle <gopalingle31102002@gmail.com>
2026-02-23 12:16:58 +0530

    eighth commit

--- EP07_FindingThePath/menu-api/data/restaurants.js
+++ EP07_FindingThePath/menu-api/data/restaurants.js
@@ -5,7 +5,7 @@ module.exports = [
     rating: 4.3,
     area: "Janta Nagar",
     costForTwo: 350,
-    image: "https://picsum.photos/id/1080/400/250",
+    image: "https://nextgalleriamalls.com/nextmusarambagh/wp-content/uploads/2024/02/pizzahut.png",
     menu: [
       { id: "101", name: "Margherita Pizza", price: 199 },
       { id: "102", name: "Veggie Supreme Pizza", price: 349 }
@@ -17,7 +17,7 @@ module.exports = [
     rating: 4.0,
     area: "Kharar",
     costForTwo: 300,
-    image: "https://picsum.photos/id/292/400/250",
+    image: "https://images.seeklogo.com/logo-png/28/3/subway-logo-png_seeklogo-287365.png",
     menu: [
       { id: "201", name: "Veg Sub", price: 180 },
       { id: "202", name: "Paneer Tikka Sub", price: 220 }
@@ -29,7 +29,7 @@ module.exports = [
     rating: 4.3,
     area: "Greater Mohali",
     costForTwo: 300,
-    image: "https://picsum.photos/id/1060/400/250",
+    image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTqvQpBee3eIJr3vgs4OLmHNKBQa5sH_m-M5Q&s",
     menu: [
       { id: "301", name: "Farm Villa Pizza", price: 299 },
       { id: "302", name: "Cheese Burst Pizza", price: 349 }
@@ -41,7 +41,7 @@ module.exports = [
     rating: 4.8,
     area: "Nirwana Square",
     costForTwo: 400,
-    image: "https://picsum.photos/id/1069/400/250",
+    image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSzTWV4otuEm2RFVsa0A2rATJwLrW4-eBcZHg&s",
     menu: [
       { id: "401", name: "Caffè Latte", price: 260 },
       { id: "402", name: "Cappuccino", price: 240 }
@@ -53,7 +53,7 @@ module.exports = [
     rating: 4.4,
     area: "Amayra Emporio",
     costForTwo: 400,
-    image: "https://picsum.photos/id/1084/400/250",
+    image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ7NvjtUsCJBW8YiUEyyStx1a4WiktoIa9hgw&s",
     menu: [
       { id: "501", name: "McAloo Tikki", price: 79 },
       { id: "502", name: "McVeggie Burger", price: 149 }
@@ -65,7 +65,7 @@ module.exports = [
     rating: 4.4,
     area: "Nirwana Square",
     costForTwo: 400,
-    image: "https://picsum.photos/id/1025/400/250",
+    image: "https://images.seeklogo.com/logo-png/42/1/kfc-logo-png_seeklogo-425726.png",
     menu: [
       { id: "601", name: "Veg Zinger Burger", price: 179 },
       { id: "602", name: "French Fries", price: 99 }
@@ -77,7 +77,7 @@ module.exports = [
     rating: 4.3,
     area: "Landran Rd",
     costForTwo: 200,
-    image: "https://picsum.photos/id/1059/400/250",
+    image: "https://e7.pngegg.com/pngimages/432/7/png-clipart-wendy-s-company-fast-food-restaurant-hamburger-mc-donald-logo.png",
     menu: [
       { id: "701", name: "Veg Burger", price: 129 },
       { id: "702", name: "Cheesy Fries", price: 149 }
@@ -89,7 +89,7 @@ module.exports = [
     rating: 4.4,
     area: "City Heart",
     costForTwo: 200,
-    image: "https://picsum.photos/id/1040/400/250",
+    image: "https://play-lh.googleusercontent.com/SeUrkLa1Ayjdu0i64XCCfabK3ybmkw4_Q2CGIDNPFHWuzsF4E7m1nv0acnRSyyeR1EA=w600-h300-pc0xffffff-pd",
     menu: [
       { id: "801", name: "Paneer Wrap", price: 180 },
       { id: "802", name: "Veg Shawarma Roll", price: 160 }
@@ -101,7 +101,7 @@ module.exports = [
     rating: 4.5,
     area: "Khanpur",
     costForTwo: 600,
-    image: "https://picsum.photos/id/1062/400/250",
+    image: "https://stores.barista.co.in/wp-content/uploads/2024/04/Barista-Circle-Logo-PNG-1000x1000-1.png",
     menu: [
       { id: "901", name: "Espresso", price: 220 },
       { id: "902", name: "Mocha", price: 260 }
@@ -113,7 +113,7 @@ module.exports = [
     rating: 4.5,
     area: "Sector 125",
     costForTwo: 250,
-    image: "https://picsum.photos/id/1035/400/250",
+    image: "https://etimg.etb2bimg.com/photo/79836447.cms",
     menu: [
       { id: "1001", name: "Dal Rice Bowl", price: 199 },
       { id: "1002", name: "Healthy Veg Thali", price: 249 }
@@ -125,7 +125,7 @@ module.exports = [
     rating: 4.7,
     area: "Sunny Enclave",
     costForTwo: 200,
-    image: "https://picsum.photos/id/1081/400/250",
+    image: "https://cdn.prod.website-files.com/62aae3a2134cd27204efacc7/62c2ee99261b69343d322026_Untitled%20design%20(13).png",
     menu: [
       { id: "1101", name: "Chocolate Truffle Cake", price: 399 },
       { id: "1102", name: "Red Velvet Cake", price: 449 }
@@ -137,7 +137,7 @@ module.exports = [
     rating: 4.0,
     area: "City Heart",
     costForTwo: 400,
-    image: "https://picsum.photos/id/1043/400/250",
+    image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSRzJvPVTnWveLWxff2KYS2TgHRxY5dWeOJRw&s",
     menu: [
       { id: "1201", name: "Paneer Pizza", price: 349 },
       { id: "1202", name: "Veg Overloaded Pizza", price: 399 }
@@ -149,7 +149,7 @@ module.exports = [
     rating: 4.4,
     area: "City Heart",
     costForTwo: 450,
-    image: "https://picsum.photos/id/1082/400/250",
+    image: "https://product-assets.faasos.io/eatsure_cms/production/0868997e-e159-4170-93f7-40239ac156bd.jpg",
     menu: [
       { id: "1301", name: "Chocolate Jar Cake", price: 199 },
       { id: "1302", name: "Butterscotch Cake", price: 349 }
@@ -161,7 +161,7 @@ module.exports = [
     rating: 4.5,
     area: "Kharar",
     costForTwo: 700,
-    image: "https://picsum.photos/id/102/400/250",
+    image: "https://play-lh.googleusercontent.com/AEXOlHdqd3z7BKxvaZCj0dbCmJiBRxjQSRUdM3OFEFMzBc-P7ZMoou0Xs6IAptYj7g",
     menu: [
       { id: "1401", name: "Chocolate Pastry", price: 220 },
       { id: "1402", name: "Garlic Bread", price: 180 }
@@ -173,7 +173,7 @@ module.exports = [
     rating: 4.3,
     area: "Sunny Enclave",
     costForTwo: 200,
-    image: "https://picsum.photos/id/1083/400/250",
+    image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTm8kFsgontWxQc-_RbSM23nOOBK5WP4lUVtg&s",
     menu: [
       { id: "1501", name: "Blueberry Cheesecake", price: 299 },
       { id: "1502", name: "New York Cheesecake", price: 349 }
@@ -185,7 +185,7 @@ module.exports = [
     rating: 4.4,
     area: "City Heart",
     costForTwo: 350,
-    image: "https://picsum.photos/id/1049/400/250",
+    image: "https://play-lh.googleusercontent.com/SeUrkLa1Ayjdu0i64XCCfabK3ybmkw4_Q2CGIDNPFHWuzsF4E7m1nv0acnRSyyeR1EA=w600-h300-pc0xffffff-pd",
     menu: [
       { id: "1601", name: "Signature Paneer Roll", price: 240 },
       { id: "1602", name: "Loaded Veg Wrap", price: 260 }
@@ -197,7 +197,7 @@ module.exports = [
     rating: 4.6,
     area: "Sector 125",
     costForTwo: 200,
-    image: "https://picsum.photos/id/1084/400/250",
+    image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQBE56sHitKzlGwf99XUzwrItioXP6l-wrUYg&s",
     menu: [
       { id: "1701", name: "Chocolate Brownie", price: 180 },
       { id: "1702", name: "Fruit Cake", price: 220 }
@@ -209,7 +209,7 @@ module.exports = [
     rating: 4.3,
     area: "Kharar",
     costForTwo: 300,
-    image: "https://picsum.photos/id/1050/400/250",
+    image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT6xJ7ty1o43qn3LmEPfE3ulLu7-5gBk72cZg&s",
     menu: [
       { id: "1801", name: "Cheese Burst Pizza", price: 349 },
       { id: "1802", name: "Garlic Cheese Bread", price: 199 }
@@ -221,7 +221,7 @@ module.exports = [
     rating: 4.5,
     area: "Sunny Enclave",
     costForTwo: 200,
-    image: "https://picsum.photos/id/1033/400/250",
+    image: "https://lh3.googleusercontent.com/PEHMn-xM0pp2vP2UOZgYMpZWKofAOzkp_pyzgyDJZsGTbZRrmIDThuyQ8rV7cDU7C-ycskru5Ceu2M0LWkLEbxCmvAumUA77c7RTmZU=w256-rw",
     menu: [
       { id: "1901", name: "Chocolate Pastry", price: 150 },
       { id: "1902", name: "Brownie", price: 180 }
@@ -233,7 +233,7 @@ module.exports = [
     rating: 4.1,
     area: "City Heart",
     costForTwo: 200,
-    image: "https://picsum.photos/id/1039/400/250",
+    image: "https://lh3.googleusercontent.com/cVcJBAXjmkesmHBdWxWBFRJZY1p819nhf3jDb8U3KYfYWgfb21ngZ-sApJefSy6q_3aCTvVlFyGRgPioe_x1XcOorcFLKDW0QnmX-EPBLA",
     menu: [
       { id: "2001", name: "Veg Meal Box", price: 199 },
       { id: "2002", name: "Paneer Meal Box", price: 249 }
